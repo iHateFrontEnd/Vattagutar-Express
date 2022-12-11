f81306dafd959db1fe91ba9da65a0b1c9b6eb229
Rushabh.R <74890980+iHateFrontEnd@users.noreply.github.com>
2022-12-11 12:11:07 +0530

    removed un-wanted code

--- routes/change-username.js
+++ routes/change-username.js
@@ -1,21 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const fs = require('fs');
-var usersFile = require('../../users.json');
 
 router.post('/', (req, res) => {
-    const username = req.body.username;
-    const userIndex = req.body.userIndex;
-
-    usersFile.users[userIndex].username = username;
-
-    fs.writeFile('../users.json', JSON.stringify(usersFile, null, 2), (err) => {
-        if (err) {
-            console.log(err);
-        }
-    });
-
-    res.end();
 });
 
 module.exports = router;
