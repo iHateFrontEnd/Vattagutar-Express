0fc89703900631e0b3632b63687bb2a6556e1765
Rushabh.R <74890980+iHateFrontEnd@users.noreply.github.com>
2022-12-11 12:08:51 +0530

    changed config file path

--- routes/create-group.js
+++ routes/create-group.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const configFile = require('../../config.json');
+const configFile = require('../config.json');
 const { MongoClient } = require('mongodb');
 require('dotenv').config();
 async function createGroup(username, groupName, res) {
